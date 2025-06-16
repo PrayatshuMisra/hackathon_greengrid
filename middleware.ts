9924d14d4d9d8ec0d3b393e1c29472ca1abc19ad
Prayatshu Misra <prayatshumisra2005@gmail.com>
2025-06-16 16:48:20 +0530

    update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,21 +4,21 @@ import type { NextRequest } from "next/server"
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
-  const supabase = createMiddlewareClient({ req, res });
-
+  const supabase = createMiddlewareClient({ req, res })
 
   const {
     data: { session },
   } = await supabase.auth.getSession()
 
-const isAuthPage = req.nextUrl.pathname.startsWith("/auth")
-const isDemo = req.cookies.get("demo")?.value === "true" || req.nextUrl.searchParams.get("demo") === "true";
-if (!session && !isDemo && !isAuthPage && req.nextUrl.pathname !== "/") {
-  const redirectUrl = req.nextUrl.clone()
-  redirectUrl.pathname = "/auth/login"
-  redirectUrl.searchParams.set("redirectedFrom", req.nextUrl.pathname)
-  return NextResponse.redirect(redirectUrl)
-}
+  const isAuthPage = req.nextUrl.pathname.startsWith("/auth")
+  const isDemo = req.cookies.get("demo")?.value === "true" || req.nextUrl.searchParams.get("demo") === "true"
+
+  if (!session && !isDemo && !isAuthPage && req.nextUrl.pathname !== "/") {
+    const redirectUrl = req.nextUrl.clone()
+    redirectUrl.pathname = "/auth/login"
+    redirectUrl.searchParams.set("redirectedFrom", req.nextUrl.pathname)
+    return NextResponse.redirect(redirectUrl)
+  }
 
   if (session && isAuthPage) {
     const redirectUrl = req.nextUrl.clone()
@@ -31,7 +31,6 @@ if (!session && !isDemo && !isAuthPage && req.nextUrl.pathname !== "/") {
 
 export const config = {
   matcher: [
-    
-    "/((?!_next/static|_next/image|favicon.ico|public).*)",
+    "/((?!_next/static|_next/image|favicon.ico|auth|public).*)",
   ],
 }
